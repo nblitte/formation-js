d4977f4d823343613baf3809d6bf3b3e5641f556
Nathalie Blitte <nblitte@bitoo.fr>
2023-03-02 09:00:33 +0100

    Projet JS

---
+++ fichier1.js
@@ -0,0 +1,11 @@
+
+const fs = require("fs");
+
+// readFileSync <- lire le contenu d'un fichier fichier 
+//writeFileSync <- écrire un fichieravec un contenu
+
+fs.writeFileSync("test.txt", "Mon premier fichier");
+console.log("Le fichier test.txt a été créé");
+
+const fileContent = fs.readFileSync("test.txt","utf-8");
+console.log(fileContent);
